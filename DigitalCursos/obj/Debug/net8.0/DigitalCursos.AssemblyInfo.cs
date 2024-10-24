fbd036352353c8f94e14c633506136a099653160
Gabriel Ferreira Carvalho <118980722+gabrieloakz@users.noreply.github.com>
2024-10-24 16:15:10 +0100

    login view

--- DigitalCursos/obj/Debug/net8.0/DigitalCursos.AssemblyInfo.cs
+++ DigitalCursos/obj/Debug/net8.0/DigitalCursos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DigitalCursos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c98752800572221e63687802d287266bca31c21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c660779afa21db506b660dea0e2d32ad8781004")]
 [assembly: System.Reflection.AssemblyProductAttribute("DigitalCursos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DigitalCursos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
